215ca52d293b2c1e13361a0c17517621919d8115
Haarieh <suporte@haarieh.com>
2015-11-17 17:54:56 -0200

    Ajuste do action mailchimp

--- mailchimp-for-wp/lead-conversion.php
+++ mailchimp-for-wp/lead-conversion.php
@@ -19,14 +19,16 @@ function mc4wp_add_lead_conversion( $form_data ) {
     }
 }
 
-function rdmc4wp_form_success( $number, $data_email, $data ) {
-	$form_data['token_rdstation'] =  get_option("rdmc4wp-token");
-	$form_data['identificador'] = get_option("rdmc4wp-identificador");
-	$form_data['form_origem'] = 'Plugin MC4WP';
-	$form_data['email'] = $data['EMAIL'];
-	if(array_key_exists('FNAME',$data)) $form_data['nome'] = $data['FNAME'];
-	mc4wp_add_lead_conversion($form_data);
-
+function action_mc4wp_after_subscribe( $data_email, $data, $number, $result ) {
+	if($result) {
+		$form_data['token_rdstation'] =  get_option("rdmc4wp-token");
+		$form_data['identificador'] = get_option("rdmc4wp-identificador");
+		$form_data['form_origem'] = 'Plugin MC4WP';
+		$form_data['email'] = $data['EMAIL'];
+		if(array_key_exists('FNAME',$data)) $form_data['nome'] = $data['FNAME'];
+		mc4wp_add_lead_conversion($form_data);
+	}
+	
 }
 
-add_action( 'mc4wp_form_success', 'rdmc4wp_form_success', 10, 3 );
+add_action( 'mc4wp_after_subscribe', 'action_mc4wp_after_subscribe', 10, 4 );
